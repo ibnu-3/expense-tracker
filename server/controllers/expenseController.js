5646daf5674ff8df24f3dd54c250ce4d6e7d87fa
ibnu-3 <ibnuturab3@gmail.com>
2025-09-09 07:53:12 +0000

    expense route

--- server/controllers/expenseController.js
+++ server/controllers/expenseController.js
@@ -4,24 +4,40 @@ import Expense from "../models/Expense.js";
 export const addExpense = async (req, res) => {
   const { icon, category, amount, date } = req.body;
   try {
-    const expense = await Expense.create({icon,category,amount:parseFloat(amount), date});
-    res.status(200).json({
-        _id:expense._id,
-        category:expense.category,
-        amount:expense.amount,
-        date:new Date(expense.date),
-        icon:expense.icon
-    })   
+    const expense = await Expense.create({
+      icon,
+      category,
+      amount: parseFloat(amount),
+      date,
+    });
+    res.status(200).json({message:"Expense created Successfully",expense});
   } catch (error) {
     res.status(500).json({ message: "server error", error: error.message });
     console.log(error.message);
   }
 };
 
-//
+//get all
 export const getExpenses = async (req, res) => {
-  
   try {
+    const expenses = await Expense.find({}).sort({ createdAt: -1 });
+    if (!expenses) {
+      return res.status(404).json({ message: "Expense not found" });
+    }
+    res.status(201).json( expenses);
+  } catch (error) {
+    res.status(500).json({ message: "server error", error: error.message });
+    console.log(error.message);
+  }
+};
+//get one
+export const getExpense = async (req, res) => {
+  try {
+    const expense = await Expense.findById(req.params.id);
+    if (!expense) {
+      return res.status(404).json({ message: "Expense not found" });
+    }
+    res.status(201).json(expense);
   } catch (error) {
     res.status(500).json({ message: "server error", error: error.message });
     console.log(error.message);
@@ -31,6 +47,32 @@ export const getExpenses = async (req, res) => {
 export const updateExpense = async (req, res) => {
   const { icon, category, amount, date } = req.body;
   try {
+    const expense = await Expense.findById(req.params.id);
+    if (!expense) {
+      return res.status(404).json({ message: "Expense not found" });
+    }
+    expense.icon= icon || expense.icon;
+    expense.category= category || expense.category;
+    expense.amount= amount || expense.amount;
+    expense.date= date || expense.amount;
+    const newExpense = await expense.save()
+    return res.status(201).json({message:'Expense Updated!',newExpense})
+  } catch (error) {
+    res.status(500).json({ message: "server error", error: error.message });
+    console.log(error.message);
+  }
+};
+
+//delete expense
+//get one
+export const deleteExpense = async (req, res) => {
+  try {
+    const expense = await Expense.findById(req.params.id);
+    if (!expense) {
+      return res.status(404).json({ message: "Expense not found" });
+    }
+    await expense.deleteOne()
+    res.status(201).json({message:"Expense deleted!"});
   } catch (error) {
     res.status(500).json({ message: "server error", error: error.message });
     console.log(error.message);
--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 
+
+import { addExpense, deleteExpense, getExpense, getExpenses, updateExpense } from "../controllers/expenseController.js";
 import { protect } from "../middleware/authMiddleware.js";
-import { addExpense } from "../controllers/expenseController.js";
 const router =express.Router()
 
-router.post('/',protect, addExpense)
-
+router.post('/',protect, addExpense);
+router.get('/:id',protect, getExpense);
+router.get('/',protect, getExpenses);
+router.delete('/:id',protect, deleteExpense);
+router.put('/:id',protect, updateExpense);
 
 export default router;
