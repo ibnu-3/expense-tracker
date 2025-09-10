5653490efa1e0be5596f35050bf4e973f7f81ca6
ibnu-3 <ibnuturab3@gmail.com>
2025-09-10 13:39:55 +0000

    rfr

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,19 +1,58 @@
 import { MdAccountBalanceWallet } from "react-icons/md";
-import DashboardLayout from '../layouts/DashboardLayout'
-import { Link } from 'react-router-dom'
+import DashboardLayout from "../layouts/DashboardLayout";
+import { data, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axiosInstance from "../utils/axios";
 
 const Dashboard = () => {
+  const [datae, setDatae]=useState({})
+  useEffect(()=>{
+    const fetchDashboardData =async () => {
+      try {
+        const response = await axiosInstance.get('/dashboard')
+        console.log(response.data)
+        const data =response.data;
+      } catch (error) {
+        // setError(error.response?.data?.message)
+        console.log(error.response?.data?.message)
+      }
+    }
+    fetchDashboardData()
+  },[])
+  console.log(data.totalIncome)
   return (
-    <DashboardLayout activeMenu='Dashboard'>
-     <div>
-      <MdAccountBalanceWallet size={25} className=""/>
-      <div className="flex flex-col gap-3">
-        <p className="text-slate-500">total balance</p>
-        <p className="text-slate-700 font-extrabold ">$2345</p>
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="block lg:flex space-y-5 items-center justify-between gap-5 my-6  rounded-md ">
+        <div className="flex gap-5 p-3 bg-white rounded-md">
+          <div className="flex items-center justify-center w-14 h-14 rounded-full bg-purple-600">
+            <MdAccountBalanceWallet size={30} className="" />
+          </div>
+          <div className="flex flex-col ">
+            <p className="text-slate-500">total balance</p>
+            <p className="text-slate-700 font-bold leading-6 text-xl ">$2345</p>
+          </div>
+        </div>
+        <div className="flex gap-5 p-3 bg-white rounded-md flex-grow ">
+          <div className="flex items-center justify-center w-14 h-14 rounded-full bg-purple-600">
+            <MdAccountBalanceWallet size={30} className="" />
+          </div>
+          <div className="flex flex-col ">
+            <p className="text-slate-500">total balance</p>
+            <p className="text-slate-700 font-bold leading-6 text-xl ">$2345</p>
+          </div>
+        </div>
+        <div className="flex gap-5 p-3 bg-white rounded-md ">
+          <div className="flex items-center justify-center w-14 h-14 rounded-full bg-purple-600">
+            <MdAccountBalanceWallet size={30} className="" />
+          </div>
+          <div className="flex flex-col ">
+            <p className="text-slate-500">total balance</p>
+            <p className="text-slate-700 font-bold leading-6 text-xl ">$2345</p>
+          </div>
+        </div>
       </div>
-     </div>
     </DashboardLayout>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
