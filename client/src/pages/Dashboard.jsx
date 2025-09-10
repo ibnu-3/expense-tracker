4934588e20407777b5c764ed8ed30376e1bf8470
ibnu-3 <ibnuturab3@gmail.com>
2025-09-10 13:39:45 +0000

    234e

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,13 +1,17 @@
-
+import { MdAccountBalanceWallet } from "react-icons/md";
 import DashboardLayout from '../layouts/DashboardLayout'
 import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
     <DashboardLayout activeMenu='Dashboard'>
-      Home
-      <Link to={'/expense'} >Expense</Link>
-      <Link to={'/income'} >Income</Link>
+     <div>
+      <MdAccountBalanceWallet size={25} className=""/>
+      <div className="flex flex-col gap-3">
+        <p className="text-slate-500">total balance</p>
+        <p className="text-slate-700 font-extrabold ">$2345</p>
+      </div>
+     </div>
     </DashboardLayout>
   )
 }
