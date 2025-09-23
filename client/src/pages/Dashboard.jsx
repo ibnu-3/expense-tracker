a4963056655ad5829cb6a6b2993ee10fe9cd1abf
ibnu-3 <ibnuturab3@gmail.com>
2025-09-23 13:08:13 +0000

    last 60 days expense

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,22 +4,35 @@ import DashboardLayout from "../components/DashboardLayout";
 import axiosInstance from "../utils/axiosInstance";
 import {
   MdAllInclusive,
+  MdOutline3gMobiledata,
   MdOutlinePayment,
   MdOutlinePayments,
+  MdTrendingDown,
 } from "react-icons/md";
 import BarChart from "../components/BarChart";
 
 const Dashboard = () => {
   const [incomes, setIncomes] = useState([]);
   const [expenses, setExpenses] = useState([]);
+  const [last60DaysExpenses, setLast60DaysExpenses] = useState([]);
+  const [last30DaysIncomes, setLast30DaysIncomes] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const incomeResponse = await axiosInstance.get("/api/incomes");
         const expenseResponse = await axiosInstance.get("/api/expenses");
+        const last60DaysExpenseResponse = await axiosInstance.get(
+          "/api/expenses/last60DaysExpense"
+        );
+        const last30DaysIncomeResponse = await axiosInstance.get(
+          "/api/incomes/last30DaysIncome"
+        );
         setIncomes(incomeResponse.data);
+        setLast30DaysIncomes(last30DaysIncomeResponse.data);
+        console.log(last30DaysIncomeResponse.data);
         setExpenses(expenseResponse.data);
+        setLast60DaysExpenses(last60DaysExpenseResponse.data);
       } catch (error) {
         console.log(error.message || "failed to fetch data");
       }
@@ -67,6 +80,25 @@ const Dashboard = () => {
         <div className="mt-4 rounded-md bg-white p-2 max-w-96 ">
           <h1 className="font-bold text-center  ">Total Overview</h1>
           <BarChart totalExpense={totalExpense} totalIncome={totalIncome} />
+        </div>
+        <div className="mt-4">
+          <h1>Recent Expenses</h1>
+          {last60DaysExpenses.length === 0 ? (
+            <p>No expenses for now</p>
+          ) : (
+            <ul className="space-y-5">
+              {last60DaysExpenses.map((item, index) => (
+                <li key={index} className="flex items-center justify-between">
+                  <div className="p-1.5 bg-red-500">
+                    <MdOutline3gMobiledata /> 
+                  </div>{" "}{item.category}
+                  <div className="px-4 py-2 flex items-center gap-2 rounded-md bg-red-100 text-red-600">
+                    -${item.amount} <MdTrendingDown />
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </DashboardLayout>
